5f646da9314a2fb661aa6d065d252262ff8ad2bd
David Charles <charled1@gmatc.matc.edu>
2022-03-06 17:46:14 -0600

    added js insert for project 1

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+//display name and title
+
+var setName = 'Myron Charles',
+    setTitle = 'Graphic Designer',
+    nameEl = document.getElementById('name'),
+    titleEl = document.getElementById('title');
+
+    nameEl.textContent = setName;
+    titleEl.textContent = setTitle;
